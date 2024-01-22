6981b7f7a8428678bcb1c490cea7cea6d36054f8
unknown <emgcalangian@gmail.com>
2024-01-22 21:01:15 +0800

    pseudo code

---
+++ counting_substrings.py
@@ -0,0 +1,7 @@
+# Write a program to find how many times substring “Emma” appears in the given string.
+
+# pseudo code
+
+# Make a variable to contain a sentence
+# Use count function to count the number of times "Emma" appeared in the sentence
+# print how many times
