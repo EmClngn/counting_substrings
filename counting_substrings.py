9e475965d802341fee28a5682c8f0302043fdf6f
unknown <emgcalangian@gmail.com>
2024-01-22 21:10:32 +0800

    added a variable containing a sentence I made

--- counting_substrings.py
+++ counting_substrings.py
@@ -3,5 +3,8 @@
 # pseudo code
 
 # Make a variable to contain a sentence
+given_sentence = ("Emma is a great person. She cooks for us in the morning that's why I always " 
+                "look forward to meeting Emma in the morning. Emma is my very loving and caring mother.")
+print(given_sentence)
 # Use count function to count the number of times "Emma" appeared in the sentence
 # print how many times
