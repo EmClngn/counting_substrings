cfcfac27230babe941d064b9df76f9580f07a949
unknown <emgcalangian@gmail.com>
2024-01-22 21:16:37 +0800

    finalized the code

--- counting_substrings.py
+++ counting_substrings.py
@@ -3,8 +3,12 @@
 # pseudo code
 
 # Make a variable to contain a sentence
+# Change the sentence if you want to test it more
 given_sentence = ("Emma is a great person. She cooks for us in the morning that's why I always " 
                 "look forward to meeting Emma in the morning. Emma is my very loving and caring mother.")
-print(given_sentence)
+
 # Use count function to count the number of times "Emma" appeared in the sentence
+emma_counter = given_sentence.count("Emma")
+
 # print how many times
+print(f'The word "Emma" appeared', emma_counter,'times in the sentence')
